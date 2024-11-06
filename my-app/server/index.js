acd4b99f453a1971722413ab406ccd4726b7b2c8
CherniIyes <cherniiyes@gmail.com>
2024-11-06 16:03:44 +0100

    touches

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const accountSid = 'AC7c4c20a5b1c3f6c32a42462b861e0a5a';
-const authToken = '6e613ce682dd23aca896dd13de085b36';
+const authToken = '1ac3760d312264b0cd8c34c62812dd5e';
 const client = twilio(accountSid, authToken);
 
 app.post("/send-message", (req, res) => {
@@ -17,7 +17,7 @@ app.post("/send-message", (req, res) => {
       client.messages.create({
             body: message,
             from: 'whatsapp:+14155238886',
-            to: 'whatsapp:+21655331742'
+            to: 'whatsapp:+21629015899'
       })
             .then((msg) => res.status(200).json({ success: true, sid: msg.sid }))
             .catch((error) => res.status(500).json({ success: false, error }));
